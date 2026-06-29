7c596dececf81770b9c0cc028a2d662637061f4f
Arthur Bricq <arthur.bricq@protonmail.com>
2026-06-29 15:10:34 +0200

    Improve projects page look

--- generator/page_builder.py
+++ generator/page_builder.py
@@ -32,6 +32,29 @@ def _format_dates(meta):
     return None
 
 
+def _parse_keywords(value):
+    """'[Chess, Rust]' -> ['Chess', 'Rust']."""
+    return [k.strip() for k in value.strip().strip("[]").split(",") if k.strip()]
+
+
+GITHUB_ICON = (
+    '<svg viewBox="0 0 16 16" width="1em" height="1em" fill="currentColor" aria-hidden="true">'
+    '<path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 '
+    '0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53'
+    '.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 '
+    '0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 '
+    '2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 '
+    '3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"/></svg>'
+)
+
+YOUTUBE_ICON = (
+    '<svg viewBox="0 0 24 24" width="1em" height="1em" fill="currentColor" aria-hidden="true">'
+    '<path d="M23.5 6.2a3 3 0 00-2.1-2.1C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.4.6A3 3 0 00.5 6.2 '
+    '31.3 31.3 0 000 12a31.3 31.3 0 00.5 5.8 3 3 0 002.1 2.1c1.9.6 9.4.6 9.4.6s7.5 0 9.4-.6a3 3 0 '
+    '002.1-2.1A31.3 31.3 0 0024 12a31.3 31.3 0 00-.5-5.8zM9.5 15.5v-7l6.5 3.5-6.5 3.5z"/></svg>'
+)
+
+
 class PageBuilder:
     """
     A class to build an html page and all its related children pages.
@@ -95,33 +118,52 @@ class PageBuilder:
             file.write(str(template))
 
     def add_html_link_to_parent_page(self, _meta):
-        children_path = _process_children_page_title(_meta)
-        title = _process_children_page_title(_meta)
-        html_list = ""
-        html_list += (
-            '<li class="project_item"><a href="./{}/{}.html">{}</a> </li>'.format(
-                self._name, title, _meta["title"]
-            )
-        )
+        slug = _process_children_page_title(_meta)
+        href = "./{}/{}.html".format(self._name, slug)
+
+        # --- left column: title, date, tags, description, links ---
+        main = '<a class="card_title" href="{}">{}</a>'.format(href, _meta["title"])
+
         dates = _format_dates(_meta)
         if dates:
-            html_list += '<p class="date">{}</p>'.format(dates)
+            main += '<span class="card_date">{}</span>'.format(dates)
+
+        # wrap title + date on one baseline row
+        main = '<div class="card_head">{}</div>'.format(main)
+
         if "keywords" in _meta:
-            html_list += '<p class="keyword">{}</p>'.format(_meta["keywords"])
+            tags = "".join(
+                '<li class="tag">{}</li>'.format(k)
+                for k in _parse_keywords(_meta["keywords"])
+            )
+            main += '<ul class="tags">{}</ul>'.format(tags)
+
+        main += '<p class="description">{}</p>'.format(_meta["description"])
+
+        links = ""
         if "github" in _meta:
-            html_list += (
-                '<p class="description">Link to <a href="{}">Github</a></p>'.format(
-                    _meta["github"]
-                )
+            links += '<a class="icon_link" href="{}">{}GitHub</a>'.format(
+                _meta["github"], GITHUB_ICON
+            )
+        if "youtube" in _meta:
+            links += '<a class="icon_link" href="{}">{}YouTube</a>'.format(
+                _meta["youtube"], YOUTUBE_ICON
+            )
+        if links:
+            main += '<div class="card_links">{}</div>'.format(links)
+
+        # --- right column: thumbnail (optional) ---
+        thumb = ""
+        if "featuredImage" in _meta:
+            thumb = '<a class="card_thumb" href="{}"><img src="{}" alt="{}"></a>'.format(
+                href, _meta["featuredImage"], _meta["title"]
             )
 
-        # Add the description
-        html_list += '<p class="description">{}</p>'.format(_meta["description"])
-        # Add the image
-        if "featuredImage" in _meta:
-            html_list += '<img src="{}" class="thumbnail">'.format(_meta["featuredImage"])
+        html = '<div class="card card_{}"><div class="card_main">{}</div>{}</div>'.format(
+            self._name, main, thumb
+        )
 
-        tag = bs4.BeautifulSoup(html_list, "html.parser")
+        tag = bs4.BeautifulSoup(html, "html.parser")
         return tag
 
 
